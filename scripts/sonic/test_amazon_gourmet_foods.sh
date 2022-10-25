d17d7cac7160d87d06fa484d9e20aafd796c5dbb
scxfjiang <sc.xfjiang@gmail.com>
2022-10-25 18:47:04 +0100

    update

--- scripts/sonic/test_amazon_gourmet_foods.sh
+++ scripts/sonic/test_amazon_gourmet_foods.sh
@@ -19,11 +19,11 @@ DATA_PATH="/home/people/22200056/workspace/dataset/amazon/amazon_grocery_and_gou
 for LOG_DIR in Sep-10-2022-17-28-52 Sep-10-2022-17-56-25 Sep-11-2022-12-40-31 Sep-11-2022-15-57-25 Sep-11-2022-19-16-36; do
     python ../../src/test.py \
         --dataset_type="amazon_grocery_and_gourmet_foods" \
-        --test_dataset_path="${DATA_PATH}/train.json" \
-        --test_dataset_path="${DATA_PATH}/val.json" \
+        --train_dataset_path="${DATA_PATH}/train.json" \
+        --val_dataset_path="${DATA_PATH}/val.json" \
         --test_dataset_path="${DATA_PATH}/test.json" \
         --word_embeds_type="glove.6B.50d" \
-        --word_embeds_path="/home/people/22200056/workspace/dataset/glove.6B/glove.6B.50d.txt" \
+        --word_embeds_path="/scratch/22200056/dataset/glove.6B/glove.6B.50d.txt" \
         --global_user_id2global_user_idx="${DATA_PATH}/global_user_id2global_user_idx.pkl" \
         --global_item_id2global_item_idx="${DATA_PATH}/global_item_id2global_item_idx.pkl" \
         --checkpoint_path="/scratch/22200056/conv_pmf_results/conv_pmf_entropy_A/epsilon_0.005/${LOG_DIR}/checkpoint/checkpoint_50.pt" \
