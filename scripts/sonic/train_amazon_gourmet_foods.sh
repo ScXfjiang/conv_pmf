c41259182781d92563bf7581ba9c8e4b09c53085
scxfjiang <sc.xfjiang@gmail.com>
2022-10-25 17:42:42 +0100

    update

--- scripts/sonic/train_amazon_gourmet_foods.sh
+++ scripts/sonic/train_amazon_gourmet_foods.sh
@@ -17,9 +17,9 @@
 # run from current directory
 cd $SLURM_SUBMIT_DIR
 
-DATA_PATH="/home/people/22200056/workspace/dataset/amazon/amazon_grocery_and_gourmet_foods1"
+DATA_PATH="/home/people/22200056/scratch/dataset/amazon/amazon_grocery_and_gourmet_foods1"
 
-for idx in 1 2 3; do
+for idx in 1; do
     for cuda_device_idx in 0 1; do
         export CUDA_VISIBLE_DEVICES=${cuda_device_idx}
         python ../../src/train.py \
@@ -28,13 +28,13 @@ for idx in 1 2 3; do
             --val_dataset_path="${DATA_PATH}/val.json" \
             --val_dataset_path="${DATA_PATH}/test.json" \
             --word_embeds_type="glove.6B.50d" \
-            --word_embeds_path="/home/people/22200056/workspace/dataset/glove.6B/glove.6B.50d.txt" \
+            --word_embeds_path="/scratch/22200056/dataset/glove.6B/glove.6B.50d.txt" \
             --global_user_id2global_user_idx="${DATA_PATH}/global_user_id2global_user_idx.pkl" \
             --global_item_id2global_item_idx="${DATA_PATH}/global_item_id2global_item_idx.pkl" \
             --shuffle=True \
             --train_batch_size=256 \
             --val_batch_size=256 \
-            --num_epoch=60 \
+            --num_epoch=10 \
             --window_size=5 \
             --n_word=128 \
             --n_factor=32 \
