6e6f8e6717d10e1da72a4c46eb2fe7950e0bb20b
scxfjiang <sc.xfjiang@gmail.com>
2022-10-26 20:54:41 +0100

    update

--- scripts/sonic/test_amazon_gourmet_foods.sh
+++ scripts/sonic/test_amazon_gourmet_foods.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/home/people/22200056/bin/zsh
 #SBATCH --job-name=conv_pmf_test
 # speficity number of nodes
 #SBATCH -N 1
@@ -14,9 +14,15 @@
 # run from current directory
 cd $SLURM_SUBMIT_DIR
 
-DATA_PATH="/home/people/22200056/workspace/dataset/amazon/amazon_grocery_and_gourmet_foods1"
+DATA_PATH="/scratch/22200056/dataset/amazon/amazon_grocery_and_gourmet_foods1"
 
-for LOG_DIR in Sep-10-2022-17-28-52 Sep-10-2022-17-56-25 Sep-11-2022-12-40-31 Sep-11-2022-15-57-25 Sep-11-2022-19-16-36; do
+CHECKPOINT_PATH="/scratch/22200056/conv_pmf_result/baseline_without_entropy/n_factor_32"
+N_FACTOR=32
+checkpoint_files=()
+for ENTRY in "${CHECKPOINT_PATH}"/*; do
+    checkpoint_files+="${ENTRY}/checkpoint/checkpoint_50.pt"
+done
+for checkpoint in ${checkpoint_files}; do
     python ../../src/test.py \
         --dataset_type="amazon_grocery_and_gourmet_foods" \
         --train_dataset_path="${DATA_PATH}/train.json" \
@@ -26,10 +32,10 @@ for LOG_DIR in Sep-10-2022-17-28-52 Sep-10-2022-17-56-25 Sep-11-2022-12-40-31 Se
         --word_embeds_path="/scratch/22200056/dataset/glove.6B/glove.6B.50d.txt" \
         --global_user_id2global_user_idx="${DATA_PATH}/global_user_id2global_user_idx.pkl" \
         --global_item_id2global_item_idx="${DATA_PATH}/global_item_id2global_item_idx.pkl" \
-        --checkpoint_path="/scratch/22200056/conv_pmf_results/conv_pmf_entropy_A/epsilon_0.005/${LOG_DIR}/checkpoint/checkpoint_50.pt" \
+        --checkpoint_path=${checkpoint} \
         --test_batch_size=256 \
         --window_size=5 \
         --n_word=128 \
-        --n_factor=32 \
+        --n_factor=${N_FACTOR} \
         --use_cuda=True
 done
