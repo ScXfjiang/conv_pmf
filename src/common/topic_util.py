4bbcc48f99051961184bf6f7cbfb17b0f58bae1b
scxfjiang <sc.xfjiang@gmail.com>
2023-01-15 15:52:12 +0000

    update

--- src/common/topic_util.py
+++ src/common/topic_util.py
@@ -7,33 +7,32 @@ from scipy.sparse import lil_matrix
 from common.dictionary import DictionaryIf
 
 
-class SparseTokenCountMat:
-    def __init__(self, dataset_path, dictionary):
-        """
-        Args:
-            dataset_path (str): Amazon dataset
-            dictionary (dictionary.DictionaryIf):
-        """
-        assert isinstance(dictionary, DictionaryIf)
+def create_sparse_token_cnt_mat(dataset_path, dictionary):
+    """
+    Args:
+        dataset_path (str): Amazon dataset
+        dictionary (dictionary.DictionaryIf):
+    """
+    assert isinstance(dictionary, DictionaryIf)
 
-        with open(dataset_path, "rb") as f:
-            num_doc = len(f.readlines())
-            voc_size = dictionary.vocab_size()
-            self.token_cnt_mat = lil_matrix((num_doc, voc_size), dtype=np.int32)
-            f.seek(0)
-            for doc_idx, line in enumerate(f):
-                tokenizer = torchtext.data.get_tokenizer("basic_english")
-                tokens = tokenizer(str(json.loads(line)["reviewText"]))
-                for token in tokens:
-                    token_idx = dictionary.word2idx(token)
-                    self.token_cnt_mat[doc_idx, token_idx] += 1
-            self.token_cnt_mat = self.token_cnt_mat.tocsr()
+    with open(dataset_path, "rb") as f:
+        num_doc = len(f.readlines())
+        voc_size = dictionary.vocab_size()
+        token_cnt_mat = lil_matrix((num_doc, voc_size), dtype=np.int32)
+        f.seek(0)
+        for doc_idx, line in enumerate(f):
+            tokenizer = torchtext.data.get_tokenizer("basic_english")
+            tokens = tokenizer(str(json.loads(line)["reviewText"]))
+            for token in tokens:
+                token_idx = dictionary.word2idx(token)
+                token_cnt_mat[doc_idx, token_idx] += 1
 
-    def save(self, save_path):
-        scipy.sparse.save_npz(save_path, self.token_cnt_mat)
+        # scipy.sparse.save_npz(save_path, token_cnt_mat)
+
+        return token_cnt_mat.tocsr()
 
 
-class NPMI:
+class NPMIUtil:
     def __init__(self, token_cnt_mat):
         # sparse matrix: [num_doc, voc_size]
         self.token_cnt_mat = token_cnt_mat.tocsc()
