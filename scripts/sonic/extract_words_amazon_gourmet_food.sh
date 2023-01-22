1571f1dba47453d872f7b910ad7a97dc6ab39fae
scxfjiang <sc.xfjiang@gmail.com>
2023-01-22 03:57:14 +0000

    update

--- scripts/sonic/extract_words_amazon_gourmet_food.sh
+++ scripts/sonic/extract_words_amazon_gourmet_food.sh
@@ -19,8 +19,9 @@ DATA_PATH="/home/people/22200056/scratch/dataset/amazon/amazon_grocery_and_gourm
 
 python ../../src/extract_words.py \
     --dataset_path="${DATA_PATH}" \
+    --token_cnt_mat="${DATA_PATH}/token_cnt_mat.npz" \
     --word_embeds_path="/scratch/22200056/dataset/glove.6B/glove.6B.50d.txt" \
-    --checkpoint_path="/scratch/22200056/experiment/conv_pmf_result/baseline_without_entropy/n_factor_32/Oct-26-2022-23-48-17-27b6bf4c-e922-4a24-8c52-9d77e1567651/checkpoint/checkpoint_50.pt" \
+    --checkpoint_path="/scratch/22200056/experiment/compare/n_factor_32_epsilon_0/scripts/sonic/log/Jan-20-2023-17-36-34-add12c04-6daf-4063-941e-6514ff6a0f2e/checkpoint/checkpoint_50.pt" \
     --batch_size=1024 \
     --window_size=5 \
     --n_word=128 \
