a0a88e466b768d79417a2d667a018cf287988cac
scxfjiang <sc.xfjiang@gmail.com>
2023-03-28 17:19:48 +0100

    update

--- src/preprocess.py
+++ src/preprocess.py
@@ -103,7 +103,7 @@ def main():
     # e.g., reviews_Grocery_and_Gourmet_Food_5.json
     parser.add_argument("--src", default="", type=str)
     # whether to remove stopwords & punctuations
-    parser.add_argument("--remove_stopwords", default="Y", type=str)
+    parser.add_argument("--clear_corpus", default="Y", type=str)
     # the directory to store the processed data
     parser.add_argument("--dst", default="", type=str)
     # used to generate token_cnt_mat.npz for NPMI
@@ -120,12 +120,12 @@ def main():
 
     preprocessor = Preprocessor(args.src, args.dst)
     # 1. text preprocessing
-    if args.remove_stopwords == "Y":
+    if args.clear_corpus == "Y":
         preprocessor.text_preprocessing()
-    elif args.remove_stopwords == "N":
+    elif args.clear_corpus == "N":
         pass
     else:
-        raise ValueError("Invalid remove_stopwords value!")
+        raise ValueError("Invalid clear_corpus value!")
     # 2. split the original Amazon dataset into train/val/test json files
     preprocessor.split_amazon(ratios=[0.8, 0.1, 0.1])
     # 3. generate global maps
