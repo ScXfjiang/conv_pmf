60efbab910d63f9b3e37bf384eebbdcba98b72ae
scxfjiang <sc.xfjiang@gmail.com>
2023-03-29 06:06:23 +0100

    update

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # this script should be run on sonic and then copy the output to other servers
-python preprocess.py \
+python ../src/preprocess.py \
     --src="/scratch/22200056/dataset/amazon/reviews_Grocery_and_Gourmet_Food_5.json" \
     --clean_corpus="T" \
     --dst="/scratch/22200056/dataset/amazon/amazon_grocery_and_gourmet_foods_clean" \
