40fd9282ccf76696b9af53988a3d15681c638fd2
scxfjiang <sc.xfjiang@gmail.com>
2023-05-05 23:39:16 +0100

    update

--- scripts/run_amazon_gourmet_foods_kay.sh
+++ scripts/run_amazon_gourmet_foods_kay.sh
@@ -17,29 +17,37 @@ cd $SLURM_SUBMIT_DIR
 
 module load cuda/11.3
 
-DATA_PATH="/ichec/work/ucd01/xfjiang/dataset/amazon/amazon_grocery_and_gourmet_foods"
+DATA_PATH="/ichec/work/ucd01/xfjiang/dataset/amazon/amazon_grocery_and_gourmet_foods_clean"
 
-for idx in 1; do
-    for cuda_device_idx in 0 1; do
-        export CUDA_VISIBLE_DEVICES=${cuda_device_idx}
-        python ../src/run.py \
-            --dataset_path="${DATA_PATH}" \
-            --word_embeds_path="/ichec/work/ucd01/xfjiang/dataset/glove.6B/glove.6B.50d.txt" \
-            --global_user_id2global_user_idx="${DATA_PATH}/global_user_id2global_user_idx.pkl" \
-            --global_item_id2global_item_idx="${DATA_PATH}/global_item_id2global_item_idx.pkl" \
-            --shuffle=True \
-            --train_batch_size=256 \
-            --val_batch_size=256 \
-            --num_epoch=60 \
-            --window_size=5 \
-            --n_word=128 \
-            --n_factor=32 \
-            --epsilon=0.0 \
-            --lr=0.1 \
-            --momentum=0.9 \
-            --weight_decay=0.0001 &
-    done
-    for JOB in $(jobs -p); do
-        wait ${JOB}
+for n_factor in 8; do
+    for epsilon in 0.0; do
+        for cuda_device_idx in 0 1; do
+            export CUDA_VISIBLE_DEVICES=${cuda_device_idx}
+            python ../src/run.py \
+                --dataset_path="${DATA_PATH}" \
+                --word_embeds_path="/ichec/work/ucd01/xfjiang/dataset/glove.6B/glove.6B.50d.txt" \
+                --global_user_id2global_user_idx="${DATA_PATH}/global_user_id2global_user_idx.pkl" \
+                --global_item_id2global_item_idx="${DATA_PATH}/global_item_id2global_item_idx.pkl" \
+                --shuffle=True \
+                --train_batch_size=256 \
+                --val_batch_size=256 \
+                --num_epoch=35 \
+                --window_size=5 \
+                --n_word=64 \
+                --n_factor=${n_factor} \
+                --epsilon=${epsilon} \
+                --lr=0.1 \
+                --momentum=0.9 \
+                --weight_decay=0.0001 \
+                --ew_batch_size=1024 \
+                --ew_least_act_num=30 \
+                --ew_k=10 \
+                --ew_token_cnt_mat_path="${DATA_PATH}/token_cnt_mat.npz" \
+                --log_dir="n_factor_${n_factor}" \
+                --log_dir_level_2="${epsilon}" &
+        done
+        for JOB in $(jobs -p); do
+            wait ${JOB}
+        done
     done
 done
