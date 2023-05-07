2a5dc0de9f4a3a436afc3ec32cf86224a26e8473
scxfjiang <sc.xfjiang@gmail.com>
2023-05-07 10:00:49 +0100

    update

--- scripts/run_amazon_gourmet_foods_sonic.sh
+++ scripts/run_amazon_gourmet_foods_sonic.sh
@@ -40,7 +40,7 @@ for n_factor in 8; do
                 --momentum=0.9 \
                 --weight_decay=0.0001 \
                 --ew_batch_size=1024 \
-                --ew_least_act_num=10 \
+                --ew_least_act_num=30 \
                 --ew_k=10 \
                 --ew_token_cnt_mat_path="${DATA_PATH}/token_cnt_mat.npz" \
                 --log_dir="n_factor_${n_factor}" \
