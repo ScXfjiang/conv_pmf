968bba99724873e8930a73d69896529a0bbd396a
scxfjiang <sc.xfjiang@gmail.com>
2023-07-17 15:38:02 +0100

    update

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # this script should be run on sonic and then copy the output to other servers
+ORIGINAL_DATASET_PATH="/scratch/22200056/dataset/amazon/reviews_Grocery_and_Gourmet_Food_5.json"
+OUTPUT_DIR="/home/people/22200056/workspace/preprocess_output"
+REF_CORPUS="/scratch/22200056/dataset/wikitext/train.metadata.jsonl"
+WORD_EMBEDDING_PATH="/scratch/22200056/dataset/glove.6B/glove.6B.50d.txt"
+
 python ../src/preprocess.py \
-    --src="/scratch/22200056/dataset/amazon/reviews_Grocery_and_Gourmet_Food_5.json" \
+    --src=${ORIGINAL_DATASET_PATH} \
     --clean_corpus="T" \
-    --dst="/scratch/22200056/dataset/amazon/amazon_grocery_and_gourmet_foods_clean" \
-    --reference="/scratch/22200056/dataset/amazon/reviews_Grocery_and_Gourmet_Food_5.json" \
-    --word_embeds="/scratch/22200056/dataset/glove.6B/glove.6B.50d.txt"
+    --dst=${OUTPUT_DIR} \
+    --reference=${REF_CORPUS} \
+    --word_embeds=${WORD_EMBEDDING_PATH}
